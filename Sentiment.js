734f49de8051621b2503de1ba9ef88c3439717fb
Shirin Mahajan <54061109+shirin05@users.noreply.github.com>
2024-09-15 14:09:44 -0700

    Add files via upload

---
+++ Sentiment.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import Sentiment from 'sentiment';
+const sentiment = new Sentiment();
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            sentimentScore: null,
+            generalSentiment: null
+        };
+        this.findSentiment = this.findSentiment.bind(this);
+    }
+
+    findSentiment(event) {
+        const result = sentiment.analyze(event.target.value)
+        this.setState({
+            sentimentScore: result.score
+        })
+        if(result.score < 0) {
+            this.setState({
+                generalSentiment: 'Negative'
+            })
+        }
+        else if (result.score > 0) {
+            this.setState({
+                generalSentiment: 'Positive'
+            })
+        }
+        else {
+            this.setState({
+                generalSentiment: 'Neutral'
+            })
+        }
+        }
+    }
+
+    render() {
+        return (
+            <div className = "App">
+                <h2> Text Sentiment Analysis in React</h2>
+                <p> Enter text for real-time analysis:</p>
+                <textarea onChange = {this.findSentiment} />
+                <p>Sentiment Score: {this.state.sentimentScore}</p>
+                <p>General Sentiment: {this.state.generalSentiment}</p>
+            </div>
+        )
+    }
+}
+
+export default App;
